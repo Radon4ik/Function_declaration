71ec02d04cc31f62edbd3464680487cfb6e79e3a
Radon4ik <92453183+Radon4ik@users.noreply.github.com>
2021-12-03 18:59:00 +0200

    added js file

--- index.js
+++ index.js
@@ -3,35 +3,52 @@ const firstNum = prompt('Enter the first digit');
 const value = prompt('Enter the value(+-*/)');
 const secondNum = prompt('Enter the second digit');
 
+// function colculate() {
+    
+// if (value === '*') {
+// const multiply = Number(firstNum) * Number(secondNum);
+// }
+// if (value === '/') { 
+// const division = Number(firstNum) / Number(secondNum);
+// }
+// if (value === '+') {
+// const plus = Number(firstNum) + Number(secondNum);
+// }
+// if (value === '-') { 
+// const minus = Number(firstNum) - Number(secondNum);
+// }   
+// }     
 
-if (value === '*') {
-const multiply = Number(firstNum) * Number(secondNum);
-console.log(` ${firstNum} * ${secondNum} = ${multiply}`);
-}
-if (value === '/') { 
-const division = Number(firstNum) / Number(secondNum);
-console.log(` ${firstNum} / ${secondNum} = ${division}`);
-}
-if (value === '+') {
-const plus = Number(firstNum) + Number(secondNum);
-console.log(` ${firstNum} + ${secondNum} = ${plus}`);
-}
-if (value === '-') { 
-const minus = Number(firstNum) - Number(secondNum);
-console.log(` ${firstNum} - ${secondNum} = ${minus}`);
-}   
-     
-            
-        
-     
-
-
-
-
-     
-        
-        
+function multiply() {
+     if (value === '*') {
+         const multi = Number(firstNum) * Number(secondNum)
+         console.log(`${firstNum} * ${secondNum} = ${multi}`)
+     }  
+ };          
+multiply();
 
 
+function division() {
+     if (value === '/') {
+         const divi = Number(firstNum) / Number(secondNum)
+         console.log(`${firstNum} / ${secondNum} = ${divi}`)
+     }  
+ };          
+division();
 
+function plus() {
+     if (value === '+') {
+         const plusNum = Number(firstNum) + Number(secondNum)
+         console.log(`${firstNum} + ${secondNum} = ${plusNum}`)
+     }  
+ }; 
 
+plus();
+
+function minus() {
+     if (value === '-') {
+         const min = Number(firstNum) - Number(secondNum)
+         console.log(`${firstNum} - ${secondNum} = ${min}`)
+     }  
+ };          
+minus();
