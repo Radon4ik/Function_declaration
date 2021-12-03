34547df61d1fcba1bced06a089bd967c36e301da
Radon4ik <92453183+Radon4ik@users.noreply.github.com>
2021-12-03 16:28:47 +0200

    added js file

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const secondNum = prompt('Enter the second digit');
 
 
 if (value === '*') {
-    const multiply = Number(firstNum) * Number(secondNum);
-    alert(multiply);
-
-    if (value === '/') { 
-    const division = Number(firstNum) / Number(secondNum);
-    alert(division);
-
-        if (value === '+') {
-    const plus = Number(firstNum) + Number(secondNum);
-    alert(plus);
-
-            if (value === '-') {
-    const minus = Number(firstNum) - Number(secondNum);
-    alert(minus);
-    
-     
-            }
-        }
-    }  
+const multiply = Number(firstNum) * Number(secondNum);
+console.log(` ${firstNum} * ${secondNum} = ${multiply}`);
 }
+if (value === '/') { 
+const division = Number(firstNum) / Number(secondNum);
+console.log(` ${firstNum} / ${secondNum} = ${division}`);
+}
+if (value === '+') {
+const plus = Number(firstNum) + Number(secondNum);
+console.log(` ${firstNum} + ${secondNum} = ${plus}`);
+}
+if (value === '-') { 
+const minus = Number(firstNum) - Number(secondNum);
+console.log(` ${firstNum} - ${secondNum} = ${minus}`);
+}   
+     
+            
+        
+     
+
 
 
 
