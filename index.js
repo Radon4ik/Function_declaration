2761632fd1552ce022054c1d93eb57aae16f782d
Radon4ik <92453183+Radon4ik@users.noreply.github.com>
2021-12-03 02:43:20 +0200

    added js file

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-function showMessage(from, text,) {
-    alert( from + ': ' + text );
+  
+const firstNum = prompt('Enter the first digit');
+const value = prompt('Enter the value(+-*/)');
+const secondNum = prompt('Enter the second digit');
+
+
+if (value === '*') {
+    const multiply = Number(firstNum) * Number(secondNum);
+    alert(multiply);
+
+    if (value === '/') { 
+    const division = Number(firstNum) / Number(secondNum);
+    alert(division);
+
+        if (value === '+') {
+    const plus = Number(firstNum) + Number(secondNum);
+    alert(plus);
+
+            if (value === '-') {
+    const minus = Number(firstNum) - Number(secondNum);
+    alert(minus);
+    
+     
+            }
+        }
+    }  
+}
+else {
+    alert('you did not enter the required settings');
 }
 
-showMessage('Anna', 'Привет!');
+
+     
+        
+        
+
+
+
+
